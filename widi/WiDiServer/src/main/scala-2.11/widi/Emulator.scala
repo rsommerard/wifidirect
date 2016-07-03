dc122ee9d7474629ab0d37c547f401ed4776082f
Romain SOMMERARD <rsommerard@gmail.com>
2016-07-03 14:12:32 +0200

    Refactor Emulator

--- widi/WiDiServer/src/main/scala-2.11/widi/Emulator.scala
+++ widi/WiDiServer/src/main/scala-2.11/widi/Emulator.scala
@@ -12,7 +12,7 @@ class Emulator(val name: String, val deviceAddress: String, val serverPort: Int,
 
   val device: Device = Device(name, deviceAddress)
 
-  lazy val adbPath = "/home/romain/Android/Sdk/platform-tools/adb"
+  val adbPath = "/home/romain/Android/Sdk/platform-tools/adb"
 
   var dnsSdServiceResponse: DnsSdServiceResponse = _
   var dnsSdTxtRecord: DnsSdTxtRecord = _
@@ -43,164 +43,183 @@ class Emulator(val name: String, val deviceAddress: String, val serverPort: Int,
 
   def start() = {
     new Thread(new Runnable {
-
       override def run(): Unit = {
         while (true) {
           println(s"$name waiting next incoming command on port $serverPort")
           var socket = serverSocket.accept
-          val oOStream: ObjectOutputStream = new ObjectOutputStream(socket.getOutputStream)
-          val oIStream: ObjectInputStream = new ObjectInputStream(socket.getInputStream)
-          val message: String  = oIStream.readObject().toString
+          implicit val oOStream: ObjectOutputStream = new ObjectOutputStream(socket.getOutputStream)
+          implicit val oIStream: ObjectInputStream = new ObjectInputStream(socket.getInputStream)
+          val message: String  = receive()
 
           println(s"$message from ${socket.getInetAddress.getHostName}:${socket.getPort}")
 
           message match {
-            case Protocol.HELLO =>
-              println(s"> ${Protocol.HELLO}")
-              oOStream.writeObject(Protocol.HELLO)
-              oOStream.flush()
-              sendStateChangedIntent()
-              sendConnectIntent(false)
-              sendThisDeviceChangedIntent()
-
-            case Protocol.CARTON =>
-              println(s"> ${Protocol.CARTON}")
-
-            case Protocol.DISCOVER_PEERS =>
-              println(s"> ${Protocol.DISCOVER_PEERS}")
-              isDiscoverable = true
-              if (register.emulators.count(e => e.isDiscoverable) > 1) {
-                register.emulators.foreach(e => e.sendPeersChangedIntent())
-              }
-
-            case Protocol.STOP_DISCOVERY =>
-              println(s"> ${Protocol.STOP_DISCOVERY}")
-              isDiscoverable = false
-              if (register.emulators.count(e => e.isDiscoverable) > 1) {
-                register.emulators.foreach(e => e.sendPeersChangedIntent())
-              }
-
-            case Protocol.CANCEL_CONNECT =>
-              println(s"> ${Protocol.CANCEL_CONNECT}")
-
-              if (wifiP2pConfig != null) {
-                oOStream.writeObject(Protocol.ACK)
-                oOStream.flush()
-
-                sendConnectIntent(false)
-                val connectedDevice = register.emulators.filter(e => e.device.deviceAddress == wifiP2pConfig.deviceAddress)
-                connectedDevice.head.sendConnectIntent(false)
-
-                wifiP2pConfig = null
-                connectedDevice.head.wifiP2pConfig = null
-              } else {
-                oOStream.writeObject(Protocol.CARTON)
-                oOStream.flush()
-              }
-
-            case Protocol.CONNECT =>
-              println(s"> ${Protocol.CONNECT}")
-              val str = oIStream.readObject().toString
-              println(str)
-
-              val jsonWifiP2pConfig = Json.parse(str)
-              implicit val wpsInfoFormat = Json.format[WpsInfo]
-              implicit val wifiP2pConfigFormat = Json.format[WifiP2pConfig]
-
-              wifiP2pConfig = Json.fromJson[WifiP2pConfig](jsonWifiP2pConfig).get
-
-              val deviceToConnect = register.emulators.filter(e => e.device.deviceAddress == wifiP2pConfig.deviceAddress)
-
-              if (deviceToConnect.nonEmpty || deviceToConnect.head.wifiP2pConfig == null ||
-                wifiP2pConfig.deviceAddress != deviceToConnect.head.device.deviceAddress) {
-
-                if (deviceToConnect.isEmpty || deviceToConnect.head.wifiP2pConfig != null) {
-                  wifiP2pConfig = null
-                  oOStream.writeObject(Protocol.CARTON)
-                  oOStream.flush()
-                } else {
-                  oOStream.writeObject(Protocol.ACK)
-                  oOStream.flush()
-
-                  if (wifiP2pConfig.groupOwnerIntent < 7) {
-                    sendConnectIntent(true, true, deviceAddress)
-                  } else {
-                    sendConnectIntent(true, false, deviceToConnect.head.deviceAddress)
-                  }
-
-                  if (wifiP2pConfig.groupOwnerIntent < 7) {
-                    deviceToConnect.head.sendConnectIntent(true, false, deviceAddress)
-                  } else {
-                    deviceToConnect.head.sendConnectIntent(true, true, deviceToConnect.head.deviceAddress)
-                  }
-                }
-              }
-
-            case Protocol.REQUEST_PEERS =>
-              println(s"> ${Protocol.REQUEST_PEERS}")
-              implicit val deviceFormat = Json.format[Device]
-              val json = Json.toJson(register.emulators.filterNot(e => e.device.eq(device)).map(e => e.device))
-              println(json)
-              oOStream.writeObject(json.toString())
-              oOStream.flush()
-
-            case Protocol.DISCOVER_SERVICES =>
-              println(s"> ${Protocol.DISCOVER_SERVICES}")
-              isDiscoverable = true
-
-              // Receive DnsSdServiceResponse from emulator
-              var str = oIStream.readObject().toString
-              println(str)
-              val jsonDnsSdServiceResponse = Json.parse(str)
-              implicit val deviceFormat = Json.format[Device]
-              implicit val dnsSdServiceResponseFormat = Json.format[DnsSdServiceResponse]
-              dnsSdServiceResponse = Json.fromJson[DnsSdServiceResponse](jsonDnsSdServiceResponse).get
-              dnsSdServiceResponse.srcDevice = device
-
-              oOStream.writeObject(Protocol.ACK)
-              oOStream.flush()
-
-              // Receive DnsSdTxtRecord from emulator
-              str = oIStream.readObject().toString
-              println(str)
-              val jsonDnsSdTxtRecord = Json.parse(str)
-              implicit val dnsSdTxtRecordFormat = Json.format[DnsSdTxtRecord]
-              dnsSdTxtRecord = Json.fromJson[DnsSdTxtRecord](jsonDnsSdTxtRecord).get
-              dnsSdTxtRecord.srcDevice = device
-
-              oOStream.writeObject(Protocol.ACK)
-              oOStream.flush()
-
-              if (register.emulators.count(e => e.isDiscoverable) > 1) {
-                register.emulators.filter(e => e.isDiscoverable).foreach(e => e.sendPeersChangedIntent())
-              }
-
-              // Send DnsSdServiceResponse to emulator
-              val dnsSdServiceResponses: List[DnsSdServiceResponse] = register.emulators.filter(e => e.isDiscoverable).filterNot(e => e.dnsSdServiceResponse == dnsSdServiceResponse).map(e => e.dnsSdServiceResponse)
-              val jsonDnsSdServiceResponses: JsValue = Json.toJson(dnsSdServiceResponses)
-              println(jsonDnsSdServiceResponses.toString())
-              oOStream.writeObject(jsonDnsSdServiceResponses.toString())
-              oOStream.flush()
-
-              var ack = oIStream.readObject().toString
-              if (Protocol.ACK == ack) {
-                // Send DnsSdTxtRecord to emulator
-                val dnsSdTxtRecords: List[DnsSdTxtRecord] = register.emulators.filter(e => e.isDiscoverable).filterNot(e => e.dnsSdTxtRecord.eq(dnsSdTxtRecord)).map(e => e.dnsSdTxtRecord)
-                val jsonDnsSdTxtRecords = Json.toJson(dnsSdTxtRecords)
-                println(jsonDnsSdTxtRecords.toString())
-                oOStream.writeObject(jsonDnsSdTxtRecords.toString())
-                oOStream.flush()
-
-                ack = oIStream.readObject().toString
-              }
-
-            case _ =>
-              println("> unknown")
+            case Protocol.HELLO => hello()
+            case Protocol.CARTON => carton()
+            case Protocol.DISCOVER_PEERS => discoverPeers()
+            case Protocol.STOP_DISCOVERY => stopDiscovery()
+            case Protocol.CANCEL_CONNECT => cancelConnect()
+            case Protocol.CONNECT => connect()
+            case Protocol.REQUEST_PEERS => requestPeers()
+            case Protocol.DISCOVER_SERVICES => discoverServices()
+            case _ => unknown()
           }
 
           socket.close()
         }
       }
     }).start()
+  }
+
+  def send(message: String)(implicit oOStream: ObjectOutputStream): Unit = {
+    oOStream.writeObject(message)
+    oOStream.flush()
+  }
+
+  def receive()(implicit oIStream: ObjectInputStream): String = {
+    oIStream.readObject().toString
+  }
+
+  def hello()(implicit oOStream: ObjectOutputStream): Unit = {
+    println(s"> ${Protocol.HELLO}")
+    send(Protocol.HELLO)
+    sendStateChangedIntent()
+    sendConnectIntent(false)
+    sendThisDeviceChangedIntent()
+  }
+
+  def carton(): Unit = {
+    println(s"> ${Protocol.CARTON}")
+  }
+
+  def discoverPeers(): Unit = {
+    println(s"> ${Protocol.DISCOVER_PEERS}")
+    isDiscoverable = true
+    if (register.emulators.count(e => e.isDiscoverable) > 1) {
+      register.emulators.foreach(e => e.sendPeersChangedIntent())
+    }
+  }
+
+  def stopDiscovery(): Unit = {
+    println(s"> ${Protocol.STOP_DISCOVERY}")
+    isDiscoverable = false
+    if (register.emulators.count(e => e.isDiscoverable) > 1) {
+      register.emulators.foreach(e => e.sendPeersChangedIntent())
+    }
+  }
+
+  def cancelConnect()(implicit oOStream: ObjectOutputStream): Unit = {
+    println(s"> ${Protocol.CANCEL_CONNECT}")
+
+    if (wifiP2pConfig == null) {
+      send(Protocol.CARTON)
+      return
+    }
+
+    send(Protocol.ACK)
+
+    sendConnectIntent(false)
+    val connectedDevice = register.emulators.filter(e => e.device.deviceAddress == wifiP2pConfig.deviceAddress)
+    connectedDevice.head.sendConnectIntent(false)
+
+    wifiP2pConfig = null
+    connectedDevice.head.wifiP2pConfig = null
+  }
+
+  def connect()(implicit oIStream: ObjectInputStream, oOStream: ObjectOutputStream): Unit = {
+    println(s"> ${Protocol.CONNECT}")
+    val str = receive()
+    println(str)
+
+    val jsonWifiP2pConfig = Json.parse(str)
+    implicit val wpsInfoFormat = Json.format[WpsInfo]
+    implicit val wifiP2pConfigFormat = Json.format[WifiP2pConfig]
+
+    wifiP2pConfig = Json.fromJson[WifiP2pConfig](jsonWifiP2pConfig).get
+
+    val deviceToConnect = register.emulators.filter(e => e.device.deviceAddress == wifiP2pConfig.deviceAddress)
+
+    if (deviceToConnect.isEmpty && deviceToConnect.head.wifiP2pConfig != null &&
+      wifiP2pConfig.deviceAddress == deviceToConnect.head.device.deviceAddress) {
+      return
+    }
+
+    if (deviceToConnect.isEmpty || deviceToConnect.head.wifiP2pConfig != null) {
+      wifiP2pConfig = null
+      send(Protocol.CARTON)
+      return
+    }
+
+    send(Protocol.ACK)
+
+    if (wifiP2pConfig.groupOwnerIntent < 7) {
+      sendConnectIntent(true, true, deviceAddress)
+      deviceToConnect.head.sendConnectIntent(true, false, deviceAddress)
+      return
+    }
+
+    sendConnectIntent(true, false, deviceToConnect.head.deviceAddress)
+    deviceToConnect.head.sendConnectIntent(true, true, deviceToConnect.head.deviceAddress)
+  }
+
+  def requestPeers()(implicit oOStream: ObjectOutputStream): Unit = {
+    println(s"> ${Protocol.REQUEST_PEERS}")
+    implicit val deviceFormat = Json.format[Device]
+    val json = Json.toJson(register.emulators.filterNot(e => e.device.eq(device)).map(e => e.device))
+    println(json)
+    send(json.toString())
+  }
+
+  def discoverServices()(implicit oIStream: ObjectInputStream, oOStream: ObjectOutputStream): Unit = {
+    println(s"> ${Protocol.DISCOVER_SERVICES}")
+    isDiscoverable = true
+
+    // Receive DnsSdServiceResponse from emulator
+    var str = receive()
+    println(str)
+    val jsonDnsSdServiceResponse = Json.parse(str)
+    implicit val deviceFormat = Json.format[Device]
+    implicit val dnsSdServiceResponseFormat = Json.format[DnsSdServiceResponse]
+    dnsSdServiceResponse = Json.fromJson[DnsSdServiceResponse](jsonDnsSdServiceResponse).get
+    dnsSdServiceResponse.srcDevice = device
+
+    send(Protocol.ACK)
+
+    // Receive DnsSdTxtRecord from emulator
+    str = receive()
+    println(str)
+    val jsonDnsSdTxtRecord = Json.parse(str)
+    implicit val dnsSdTxtRecordFormat = Json.format[DnsSdTxtRecord]
+    dnsSdTxtRecord = Json.fromJson[DnsSdTxtRecord](jsonDnsSdTxtRecord).get
+    dnsSdTxtRecord.srcDevice = device
+
+    send(Protocol.ACK)
+
+    if (register.emulators.count(e => e.isDiscoverable) > 1) {
+      register.emulators.filter(e => e.isDiscoverable).foreach(e => e.sendPeersChangedIntent())
+    }
+
+    // Send DnsSdServiceResponse to emulator
+    val dnsSdServiceResponses: List[DnsSdServiceResponse] = register.emulators.filter(e => e.isDiscoverable).filterNot(e => e.dnsSdServiceResponse == dnsSdServiceResponse).map(e => e.dnsSdServiceResponse)
+    val jsonDnsSdServiceResponses: JsValue = Json.toJson(dnsSdServiceResponses)
+    println(jsonDnsSdServiceResponses.toString())
+    send(jsonDnsSdServiceResponses.toString())
+
+    var ack = receive()
+    if (Protocol.ACK != ack) {
+      return
+    }
+
+    // Send DnsSdTxtRecord to emulator
+    val dnsSdTxtRecords: List[DnsSdTxtRecord] = register.emulators.filter(e => e.isDiscoverable).filterNot(e => e.dnsSdTxtRecord.eq(dnsSdTxtRecord)).map(e => e.dnsSdTxtRecord)
+    val jsonDnsSdTxtRecords = Json.toJson(dnsSdTxtRecords)
+    println(jsonDnsSdTxtRecords.toString())
+    send(jsonDnsSdTxtRecords.toString())
+
+    ack = receive()
+  }
+
+  def unknown(): Unit = {
+    println("> unknown")
   }
 }
