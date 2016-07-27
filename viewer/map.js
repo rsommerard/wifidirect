5af4057f12ce78b020edc84ecfe86dd4f13f4cda
Romain SOMMERARD <rsommerard@gmail.com>
2016-07-27 16:08:26 +0200

    Refactor viewer

--- viewer/map.js
+++ viewer/map.js
@@ -35,13 +35,14 @@ function initMap() {
       var positions = JSON.parse(xhttp.responseText);
       var cntr = computeCenter(positions);
 
-      if ((isMapInitialized === false) || ((cntr.lat !== _map.center.lat()) && (cntr.lng !== _map.center.lng()))) {
+      if ((isMapInitialized === false)) { // || ((cntr.lat !== _map.center.lat()) && (cntr.lng !== _map.center.lng()))) {
         console.log("Drawing map...");
 
         _map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 15,
+          zoom: 14,
           center: cntr
         });
+
 
         isMapInitialized = true;
       }
@@ -55,11 +56,13 @@ function initMap() {
       }
     }
   };
+
   xhttp.open('GET', URL + '/locations');
   xhttp.send();
 
   if (isIntervalSetted === false) {
-    setInterval(initMap, 3000);
+    // refresh every 30 sec
+    setInterval(initMap, 30000);
     isIntervalSetted = true;
   }
 }
@@ -88,14 +91,14 @@ function deleteCircles() {
 
 function addCircle(pos) {
     var circle = new google.maps.Circle({
-	strokeColor: '#ff8080',
-	strokeOpacity: 0.8,
-	strokeWeight: 2,
-	fillColor: '#ff8080',
-	fillOpacity: 0.35,
-	center: {lat: pos.lat, lng: pos.lon},
-	radius: 200,
-	map: _map
+      strokeColor: '#ff8080',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#ff8080',
+      fillOpacity: 0.35,
+      center: {lat: pos.lat, lng: pos.lon},
+      radius: 200,
+      map: _map
     });
 
     circles.push(circle);
