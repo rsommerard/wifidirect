4c7276ed07bbdef1d4d99893059a1d08d5500c7e
Romain SOMMERARD <rsommerard@gmail.com>
2016-07-27 10:04:41 +0200

    Fix condition bug

--- multi-host/dclean-soft.py
+++ multi-host/dclean-soft.py
@@ -17,7 +17,7 @@ process = subprocess.Popen(['docker', 'ps', '-a'], stdout=subprocess.PIPE)
 output = str(process.communicate()[0], 'UTF-8')
 lines = output.strip().split('\n')
 for l in lines:
-    if 'rsommerard' or 'weaveworks' in l:
+    if ('rsommerard' in l) or ('weaveworks' in l):
         container_id = l.split()[0]
         process = subprocess.Popen(['docker', 'kill', container_id], stdout=subprocess.PIPE)
         process.wait()
@@ -27,7 +27,7 @@ process = subprocess.Popen(['docker', 'ps', '-a'], stdout=subprocess.PIPE)
 output = str(process.communicate()[0], 'UTF-8')
 lines = output.strip().split('\n')
 for l in lines:
-    if 'rsommerard' or 'weaveworks' in l:
+    if ('rsommerard' in l) or ('weaveworks' in l):
         container_id = l.split()[0]
         process = subprocess.Popen(['docker', 'rm', container_id], stdout=subprocess.PIPE)
         process.wait()
